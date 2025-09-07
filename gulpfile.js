e7dcbf69d3767e385c0713cf4b48fe5d0136f1cc
Matematuka <143654489+Matematuka@users.noreply.github.com>
2025-09-07 17:01:12 +0300

    header

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,5 @@
-// gulpfile.js
 import gulp from "gulp";
-import * as sass from "sass"; // сучасний імпорт
+import * as sass from "sass";
 import gulpSass from "gulp-sass";
 import cleanCSS from "gulp-clean-css";
 import autoprefixer from "gulp-autoprefixer";
@@ -11,30 +10,30 @@ const { src, dest, watch, series } = gulp;
 const sassCompiler = gulpSass(sass);
 const browserSync = browserSyncLib.create();
 
-// Шляхи до файлів
+
 const files = {
   scssPath: "src/scss/**/*.scss",
   svgIcons: "src/images/icons/*.svg",
 };
 
-// Конфігурація для SVG спрайту
+
 const svgConfig = {
   mode: {
     symbol: {
-      dest: ".", // вихідна папка для спрайту
-      sprite: "sprite.svg", // ім’я файлу спрайту
+      dest: ".", 
+      sprite: "sprite.svg", 
     },
   },
 };
 
-// Завдання: генерація SVG спрайту
+
 export const svgTask = () => {
   return src(files.svgIcons)
     .pipe(svgSprite(svgConfig))
     .pipe(dest("dist/images/"));
 };
 
-// Завдання: SCSS → CSS
+
 export const scssTask = () => {
   return src(files.scssPath)
     .pipe(sassCompiler().on("error", sassCompiler.logError))
@@ -49,7 +48,7 @@ export const scssTask = () => {
     .pipe(browserSync.stream());
 };
 
-// Локальний сервер з live-reload
+
 export const browserSyncServe = (cb) => {
   browserSync.init({
     server: { baseDir: "./" },
@@ -58,12 +57,12 @@ export const browserSyncServe = (cb) => {
   cb();
 };
 
-// Слідкування за змінами
+
 export const watchTask = () => {
-  watch("*.html").on("change", browserSync.reload); // оновлення HTML
-  watch([files.scssPath], scssTask); // оновлення SCSS
-  watch(files.svgIcons, svgTask).on("change", browserSync.reload); // оновлення SVG спрайту
+  watch("*.html").on("change", browserSync.reload); 
+  watch([files.scssPath], scssTask); 
+  watch(files.svgIcons, svgTask).on("change", browserSync.reload); 
 };
 
-// Експорт завдання по замовчуванню
+
 export default series(svgTask, scssTask, browserSyncServe, watchTask);
